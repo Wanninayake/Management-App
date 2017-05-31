ab4878d36967c3fe79dd4dc71cf2038a369544bb
Wanninayake <wmtpw19@gmail.com>
2017-05-31 06:02:03 +0530

    1.0 Management app-android

---
+++ app/src/main/java/com/example/user/managementapp/MoveAppoCalendar.java
@@ -0,0 +1,55 @@
+package com.example.user.managementapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CalendarView;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.List;
+
+public class MoveAppoCalendar extends AppCompatActivity {
+
+    private Button btnmove;
+    private CalendarView calendarView;
+    public static String curDate, num, text;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_move_appo_calendar);
+
+        btnmove =(Button)findViewById(R.id.btnmove);
+        calendarView =(CalendarView)findViewById(R.id.Calendar);
+        final DataBase databaseHelper = DataBase.getInstance(this);
+
+        Calendar date = Calendar.getInstance();
+        SimpleDateFormat sdf = new SimpleDateFormat("d-M-yyyy");
+
+        curDate = sdf.format(date.getTime());
+
+        calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange(CalendarView view, int year, int month,int dayOfMonth) {
+                int d = dayOfMonth;
+                int m = month+1;
+                int y = year;
+                curDate =String.valueOf(d)+"-"+String.valueOf(m)+"-"+String.valueOf(y);
+            }
+        });
+
+        Bundle extras = getIntent().getExtras();
+        if (extras != null) {
+            num = getIntent().getStringExtra("num");
+        }
+
+        btnmove.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                databaseHelper.updateDate(new Appo(Integer.valueOf(num),curDate));
+            }
+        });
+    }
+
+}
---
+++ app/src/main/java/com/example/user/managementapp/ViewAndEdit.java
@@ -0,0 +1,77 @@
+package com.example.user.managementapp;
+
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ViewAndEdit extends AppCompatActivity {
+
+    private TextView list;
+    private Button btnview;
+    static String cdate ;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_and_edit);
+
+        list =(TextView) findViewById(R.id.listappo);
+        btnview =(Button)findViewById(R.id.btnview);
+
+        final DataBase databaseHelper = DataBase.getInstance(this);
+
+        Bundle extras = getIntent().getExtras();
+        if (extras != null) {
+            cdate = getIntent().getStringExtra("cdate");
+        }
+
+        List<Appo> appos = databaseHelper.getAllData();
+        for (Appo app : appos) {
+            if (app.getDay().equals(cdate)) {
+                String text = app.getId() + ". " + app.getTime() + " - " + app.getTitle();
+                //Log.d("Name:",log);
+                list.setText(text);
+            }
+        }
+
+        btnview.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                final EditText input = new EditText(ViewAndEdit.this);
+                LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(
+                        LinearLayout.LayoutParams.MATCH_PARENT,
+                        LinearLayout.LayoutParams.MATCH_PARENT);
+                input.setLayoutParams(lp);
+                new AlertDialog.Builder(ViewAndEdit.this)
+                        .setTitle("Prompt")
+                        .setMessage("Enter the number to edit the Appointment:-")
+                        .setView(input)
+                        .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int which) {
+                                Intent intent=new Intent(ViewAndEdit.this, Edit.class);
+                                intent.putExtra("num",input.getText().toString());
+                                startActivity(intent);
+                            }
+                        })
+                        .setIcon(android.R.drawable.ic_dialog_alert)
+                        .show();
+
+
+            }
+        });
+
+    }
+}
